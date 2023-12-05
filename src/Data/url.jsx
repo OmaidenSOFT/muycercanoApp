9954709023cf66b137d58a36ca5a5221d40e8ff5
OmaidenSOFT <omar.rojas@sofka.com.co>
2023-12-05 10:19:11 -0500

    url pdr

--- src/Data/url.jsx
+++ src/Data/url.jsx
@@ -1,3 +1,3 @@
-// const url = 'http://186.154.144.132:82/pprotecc/muyCercanoBackend/api/MuyCercano/'
-const url = 'http://localhost:12911/api/MuyCercano/'
+const url = 'http://186.154.144.132:82/pprotecc/muyCercanoBackend/api/MuyCercano/'
+// const url = 'http://localhost:12911/api/MuyCercano/'
 export default url
